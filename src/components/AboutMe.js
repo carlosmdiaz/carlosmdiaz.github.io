4725308f5b4397e2ba20bf5c93842aa7c5480216
carlosmdiaz <carlosdiaz3979@gmail.com>
2020-04-19 20:07:56 -0400

    This commit has the first structure of my Web Page. I am using React Router for the links.

---
+++ src/components/AboutMe.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function AboutMe() {
+  return (
+    <div >
+      <h1>About Me</h1>
+    </div>
+  );
+}
+
+export default AboutMe;
---
+++ src/components/Navigation.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Style.css';
+
+function Navigation() {
+  return (
+
+    <nav className="nav">
+        <h3>Carlos Diaz</h3>
+        <ul className="nav-Links">
+            <li>Home</li>
+            <li>About Me</li>
+            <li>Projects</li>
+            <li>Contact Me</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navigation;
