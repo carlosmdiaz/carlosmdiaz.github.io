a62746c8fbcd712af712c6c06f4197a58b415f79
carlosmdiaz <carlosdiaz3979@gmail.com>
2020-04-20 00:41:36 -0400

    I changed the words in the Navigation bar to be links, that way when you clicked them you can actually go where they say. Also, I use Link (the React Router property).

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,16 +1,41 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import './Style.css';
 
 function Navigation() {
+
+    const stl ={
+        color: 'white'
+    };
+
   return (
 
     <nav className="nav">
         <h3>Carlos Diaz</h3>
         <ul className="nav-Links">
-            <li>Home</li>
-            <li>About Me</li>
-            <li>Projects</li>
-            <li>Contact Me</li>
+            <Link 
+                className="links-stl"
+                style={stl} to='/'>
+                    <li className="link-Container">Home</li>
+            </Link>
+
+            <Link
+                className="links-stl"
+                style={stl} to='/aboutme'>
+                    <li>About Me</li>
+            </Link>
+
+            <Link 
+                className="links-stl"
+                style={stl} to='/projects'>
+                    <li>Projects</li>
+            </Link>
+
+            <Link 
+                className="links-stl"
+                style={stl} to='/contactme'>
+                    <li>Contact Me</li>
+            </Link>
         </ul>
     </nav>
   )
